e9cbb1638ba126da27bbfae9a8dd46d76d5c6769
Kamila Sieczkowska <kamisia@student.edu>
2017-11-22 13:13:23 +0100

    working: timer, points and images shuffle

--- src/js/components/game.jsx
+++ src/js/components/game.jsx
@@ -1,34 +1,105 @@
 import React from 'react';
 import {Footer} from './footer.jsx';
 
-class YourCart extends React.Component {
+
+class Timer extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            time: 10,
+            isPaused: this.props.isPaused
+        }
+    }
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            isPaused: nextProps.isPaused
+        })
+
+        if(nextProps.resetTime){
+            clearInterval(this.counter);
+            
+            this.setState({
+                time: 10
+            })
+            this.startTimer();
+        }
+    }
+
     render(){
         return (
+            <p className="timer">Czas: <span id="timer">00:{this.state.time < 10 ? '0'+this.state.time : this.state.time}</span></p>
+        )
+    }
+
+    startTimer = () => {
+        this.counter = setInterval(()=> {
+            
+            if(!this.state.isPaused){
+                this.setState({
+                    time: this.state.time - 1
+                })
+                if(this.state.time <= 0){
+                    clearInterval(this.counter)
+                    this.props.handleOpponentPoints();
+                } 
+            }
+         
+        },1000)
+    }
+
+    componentDidMount(){
+        this.startTimer();
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.counter);
+    }
+}
+
+class Points extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            opponentPoints: 0,
+            userPoints: 0,
+        }
+    }
+
+    componentWillReceiveProps(nextProps){
+        this.setState({
+            opponentPoints: nextProps.opponentPoints,
+            userPoints: nextProps.userPoints
+        })
+    }
+  
+    render(){
+        return(
+            <p className="points">Punkty: <span id="points">{this.state.opponentPoints}:{this.state.userPoints}</span></p>
+        )
+    }
+}
+
+class YourCart extends React.Component {
+
+    render(){
+        console.log(this.props.userImages)
+
+        let cartImages = this.props.userImages.map((el,index)=> {
+            
+            return (
+                <div className={`image img${index+1}`} key={index+1}>
+                    <img onClick={e => this.props.handleUserPoints(e)} src={`../images/${el}`} alt={`${el}`} />
+                </div>
+                
+            )
+        })
+
+        return (
             <div className="cart user__cart">
-                <div className="image img1" id="img1">
-                    <img src="../images/bear.png" alt="obrazek"/>
-                </div>
-                <div className="image img2" id="img2">
-                    <img src="../images/bottle.png" alt="obrazek"/>
-                </div>
-                <div className="image img3" id="img3">
-                    <img src="../images/crown.png" alt="obrazek"/>
-                </div>
-                <div className="image img4" id="img4">
-                    <img src="../images/disc.png" alt="obrazek"/>
-                </div>
-                <div className="image img5" id="img5">
-                    <img src="../images/duck.png" alt="obrazek"/>
-                </div>
-                <div className="image img6" id="img6">
-                    <img src="../images/vader.png" alt="obrazek"/>
-                </div>
-                <div className="image img7" id="img7">
-                    <img src="../images/star.png" alt="obrazek"/>
-                </div>
-                <div className="image img8" id="img8">
-                    <img src="../images/fries.png" alt="obrazek"/>
-                </div>
+
+                {cartImages}
         
                 <p>Twoja karta</p>
             </div>
@@ -39,32 +110,22 @@ class YourCart extends React.Component {
 
 class OpponentCart extends React.Component {
     render(){
+
+        let opponentCartImages = this.props.opponentImages.map((el,index)=> {
+            
+            return (
+                <div className={`image img${index+1}`} key={index+1}>
+                    <img src={`../images/${el}`} alt={`${el}`} />
+                </div>
+            )
+        })
+
+
         return (
             <div className="cart opponent__cart">
-                <div className="image img1" id="img1">
-                    <img src="../images/frankie.png" alt="obrazek"/>
-                </div>
-                <div className="image img2" id="img2">
-                    <img src="../images/bird.png" alt="obrazek"/>
-                </div>
-                <div className="image img3" id="img3">
-                    <img src="../images/cat.png" alt="obrazek"/>
-                </div>
-                <div className="image img4" id="img4">
-                    <img src="../images/earth.png" alt="obrazek"/>
-                </div>
-                <div className="image img5" id="img5">
-                    <img src="../images/girl.png" alt="obrazek"/>
-                </div>
-                <div className="image img6" id="img6">
-                    <img src="../images/ostrich.png" alt="obrazek"/>
-                </div>
-                <div className="image img7" id="img7">
-                    <img src="../images/fork.png" alt="obrazek"/>
-                </div>
-                    <div className="image img8" id="img8">
-                <img src="../images/bear.png" alt="obrazek"/>
-                </div>
+                
+                {opponentCartImages}
+            
             </div>
         )
     }
@@ -72,33 +133,91 @@ class OpponentCart extends React.Component {
 
 
 class Game extends React.Component {
-    
+    constructor(props){
+        super(props);
+
+        this.state = {
+            isPaused: false,
+            resetTime: false,
+            opponentPoints: 0,
+            userPoints: 0,
+            images: ['bear.png', 'bird.png', 'bolid.png', 'bottle.png', 'cat.png', 'crown.png', 'disc.png', 'duck.png', 'earth.png', 'food.png', 'fork.png', 'frankie.png', 'fries.png', 'frog.png', 'girl.png', 'hat.png', 'ostrich.png', 'scissors.png', 'spaceship.png', 'star.png', 'tree.png', 'vader.png']
+        }
+    }
+
+    handlePause = () => {
+        this.setState({
+            isPaused: !this.state.isPaused
+        })  
+    }
+
+    handleOpponentPoints = () => {
+        this.setState({
+            opponentPoints:  this.state.opponentPoints + 1
+        })
+    }
+
+    handleUserPoints = (e) => {
+   
+        
+        
+        this.setState({
+            userPoints: this.state.userPoints + 1,
+            resetTime: true
+        })
+    }
+
+    shuffle = (arr) => {
+        var i,
+            j,
+            temp;
+        for (i = arr.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+        return arr;    
+    };
 
     render() {
+
+        let shuffleTab = this.shuffle(this.state.images);
+        let userImages = shuffleTab.slice(0,8);
+        let opponentImages = shuffleTab.slice(7,15);
+
+        console.log(userImages, opponentImages)
+
         return (
             <div>
                 <div className="background__game">
-        <div className="container_game">
-            <div className="game">
-                <div className="game_info">
-                    <p className="timer">Czas: <span id="timer">00:10</span></p>  
-                         
-                    <p className="points">Punkty: <span id="points">0:0</span></p>
-                </div>
+                    <div className="container_game">
+                        <div className="game">
+                            <div className="game_info">
+                                <Timer isPaused={this.state.isPaused}
+                                    resetTime={this.state.resetTime}
+                                    handleOpponentPoints={this.handleOpponentPoints}/>  
+                                    
+                                <Points opponentPoints={this.state.opponentPoints}
+                                    userPoints={this.state.userPoints}
+                                    handleUserPoints={this.handleUserPoints}/>
+
+                            </div>
                 
-                <YourCart />
-                <OpponentCart />
-              
-                
-                <div className="btn_box_pause">
-                    <a href="#" className="btn_next">Następna karta</a>
-                    <a href="#" className="btn_pause">Pauza</a>
+                            <YourCart userImages={userImages}
+                                handleUserPoints={this.handleUserPoints}/>
+                            <OpponentCart opponentImages={opponentImages}/>
+                        
+                            
+                            <div className="btn_box_pause">
+                                <a onClick={this.handlePause} className="btn_pause">Pauza</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </div>
 
                 <Footer />
+
             </div>
         )
     }
